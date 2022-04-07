ff0c32694b2b188334ff3193b48ed30fddcd6e62
Lucie <25330882+lihbr@users.noreply.github.com>
2022-04-07 11:44:26 +0200

    Initial commit

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "vitest";
+
+import * as lib from "../src";
+
+// TODO: Dummy test, meant to be removed when real tests come in
+test("exports something", () => {
+	expect(lib).toBeTruthy();
+});
